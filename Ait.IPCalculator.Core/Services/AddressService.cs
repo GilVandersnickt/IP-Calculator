6e5764bf4077e89e5f9e3ac8688bdcba96f196ac
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-03 17:37:24 +0200

    Update address service with SetAddress method

--- Ait.IPCalculator.Core/Services/AddressService.cs
+++ Ait.IPCalculator.Core/Services/AddressService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ait.IPCalculator.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,21 @@ namespace Ait.IPCalculator.Core.Services
 {
     public class AddressService
     {
+        private readonly SubnetService subnetService;
+        private readonly ConverterService converterService;
+
+        public AddressService()
+        {
+            subnetService = new SubnetService();
+            converterService = new ConverterService();
+        }
+        private Address SetAddress(string input)
+        {
+            string[] splitAddress = input.Split('.');
+            byte[] convertedArray = splitAddress.Select(x => byte.Parse(x)).ToArray();
+
+            return new Address(convertedArray[0], convertedArray[1], convertedArray[2], convertedArray[3]);
+        }
+
     }
 }
