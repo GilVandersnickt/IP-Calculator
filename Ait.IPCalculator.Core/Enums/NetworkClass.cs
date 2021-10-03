da3d98d71bd33813223ade87077a28de60f9158d
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-03 16:54:27 +0200

    Add enum for networkclass

---
+++ Ait.IPCalculator.Core/Enums/NetworkClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ait.IPCalculator.Core.Enums
+{
+    public enum NetworkClass
+    {
+        A, B, C, D, E
+    }
+}
