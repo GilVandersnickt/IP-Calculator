80b12b7494e7979138c7423a93518f3811089195
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-03 17:06:13 +0200

    Update converter service with ConvertByteToBitArray method

--- Ait.IPCalculator.Core/Services/ConverterService.cs
+++ Ait.IPCalculator.Core/Services/ConverterService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,34 @@ namespace Ait.IPCalculator.Core.Services
 {
     public class ConverterService
     {
+
         private static byte[] SplitAddress(string address)
         {
             string[] splitAddress = address.Split('.');
             byte[] convertedArray = splitAddress.Select(x => byte.Parse(x)).ToArray();
             return convertedArray;
         }
+        private static BitArray ConvertByteToBitArray(byte input)
+        {
+            BitArray temporaryBitArray = new BitArray(BitConverter.GetBytes(input));
+            BitArray bitArray = new BitArray(8);
+            // Trim bitArray from 16 to 8 bits
+            for (int i = 0; i < 8; i++)
+                bitArray[i] = temporaryBitArray[i];
 
+            ReverseBitArray(bitArray);
+            return bitArray;
+        }
+        private static void ReverseBitArray(BitArray input)
+        {
+            int length = input.Length;
+
+            for (int i = 0; i < (length / 2); i++)
+            {
+                bool bit = input[i];
+                input[i] = input[length - i - 1];
+                input[length - i - 1] = bit;
+            }
+        }
     }
 }
