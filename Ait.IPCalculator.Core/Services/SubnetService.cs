c4dd190a31f333043c34d04662f6431c08828ccf
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-03 17:25:08 +0200

    Update subnet service with GetSubnetMaskBytes method

--- Ait.IPCalculator.Core/Services/SubnetService.cs
+++ Ait.IPCalculator.Core/Services/SubnetService.cs
@@ -8,5 +8,29 @@ namespace Ait.IPCalculator.Core.Services
 {
     public class SubnetService
     {
+        public SubnetService()
+        {
+        }
+
+        private List<byte> GetSubnetMaskBytes()
+        {
+            List<byte> bytes = new List<byte>();
+            string binary = "";
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (j < i)
+                        binary += "1";
+                    else
+                        binary += "0";
+                }
+                bytes.Add(Convert.ToByte(binary, 2));
+                binary = "";
+            }
+            return bytes;
+        }
+
     }
 }
