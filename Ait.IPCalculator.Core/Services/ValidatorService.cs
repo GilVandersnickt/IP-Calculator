80b51647c8fd4ae81e75701a0355907b0a266b9c
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-03 17:18:26 +0200

    Add class for validator service

---
+++ Ait.IPCalculator.Core/Services/ValidatorService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ait.IPCalculator.Core.Services
+{
+    public class ValidatorService
+    {
+        public ValidatorService()
+        {
+
+        }
+        public bool ValidateIpAddress(string ipAddress)
+        {
+            if (String.IsNullOrWhiteSpace(ipAddress))
+                return false;
+
+            string[] splitAddresses = ipAddress.Split('.');
+            if (splitAddresses.Length != 4)
+                return false;
+
+            return splitAddresses.All(splitAddress => byte.TryParse(splitAddress, out byte result));
+        }
+    }
+}
