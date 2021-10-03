2b0a3eac6fddfd5764b1bd30c9b49412490f1840
gilvandersnickt <gil.vandersnickt@student.howest.be>
2021-10-03 17:53:52 +0200

    Update address service with GetNetworkType method

--- Ait.IPCalculator.Core/Services/AddressService.cs
+++ Ait.IPCalculator.Core/Services/AddressService.cs
@@ -80,6 +80,31 @@ namespace Ait.IPCalculator.Core.Services
             else
                 return NetworkClass.E.ToString();
         }
+        public string GetNetworkType(string input)
+        {
+            Address address = SetAddress(input);
+            // Private addresses : 10.X.X.X, 172.16.X.X – 172.31.X.X, 192.168.X.X
+            if (address.FirstOctet == 10 || (address.FirstOctet == 172 && (16 <= address.SecondOctet && address.SecondOctet <= 31) || (address.FirstOctet == 192 && address.SecondOctet == 168)))
+                return NetworkType.Private.ToString();
+            // Shared address space : 100.64.0.1 - 100.127.255.254
+            else if (address.FirstOctet == 100 && (64 <= address.SecondOctet && address.SecondOctet <= 127) && (1 <= address.FourthOctet && address.FourthOctet <= 254))
+                return NetworkType.Shared.ToString();
+            // Loopback addresses : 127.X.X.X
+            else if (address.FirstOctet == 127)
+                return NetworkType.Loopback.ToString();
+            // Link-local addresses : 169.254.X.X
+            else if (address.FirstOctet == 169 && address.SecondOctet == 254)
+                return NetworkType.LinkLocal.ToString();
+            // Experimental addresses : 240.X.X.X – 255.X.X.X
+            else if (240 <= address.FirstOctet && address.FirstOctet <= 255)
+                return NetworkType.Experimental.ToString();
+            // Test-net addresses : 192.0.2.0 - 192.0.2.255
+            else if (address.FirstOctet == 192 && address.SecondOctet == 0 && address.ThirdOctet == 2)
+                return NetworkType.TestNet.ToString();
+            // Public addresses 
+            else
+                return NetworkType.Public.ToString();
+        }
 
         private Address SetAddress(string input)
         {
